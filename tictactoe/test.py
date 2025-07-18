47fa26eb1a03acf76c6fdb76569bdcafb4bcc2ca
mvipat <mvipat@gmail.com>
2025-07-18 12:18:54 -0500

    Add files via upload

---
+++ tictactoe/test.py
@@ -0,0 +1,10 @@
+import tictactoe as ttt
+X = "X"
+O = "O"
+EMPTY = None
+
+board = [[EMPTY, EMPTY, EMPTY],
+         [EMPTY, EMPTY, EMPTY],
+         [EMPTY, EMPTY, EMPTY]]
+print(ttt.player(board))
+# should return X
